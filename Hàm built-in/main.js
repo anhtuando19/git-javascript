d6d89e71fd9d2fc0c769412e3d183739fe7f4dde
anhtuan191503 <leanhtuon1911@gmail.com>
2022-11-10 21:07:01 +0700

    javascript

---
+++ Hàm built-in/main.js
@@ -0,0 +1,23 @@
+var fullName = 'Lê Anh Tuấn'
+
+console.log(fullName);
+
+//confirm dùng để check
+
+confirm('xác nhận đủ tuổi')
+
+//prompt dùng để check cho người dùng nhập
+
+prompt('xác nhận đủ tuổi')
+
+// setTimeout(function) được chuyền vào bởi function set thơi gian hoạt dồng
+
+setTimeout(function() {
+  alert('hello world')
+}, 1000)
+
+//setInterval được chuyền cào bởi function chạy liên tục theo thời gian 
+
+setInterval(function() {
+    console.log('đây là log' + Math.random());
+}, 1000);
